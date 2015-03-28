f55e7b9ba51416275cbe1d653783d4de185e7fef
Robert Byrne <mr.rob.byrne@gmail.com>
2015-03-28 06:02:22 -0400

    Commented MyInteger file
    Commented MyInteger file

--- Lab2_2/MyInteger.java
+++ Lab2_2/MyInteger.java
@@ -19,6 +19,7 @@ class MyInteger
     return temp;
   }
 
+  // return true if number is even 
   public boolean isEven()
   {
     if (number % 2 == 0)
@@ -27,6 +28,7 @@ class MyInteger
       return false;
   }
 
+  // return true if number is odd
   public boolean isOdd()
   {
     if (number % 2 == 1)
@@ -35,19 +37,22 @@ class MyInteger
       return false;
   }
 
+  // return true if number is prime
   public boolean isPrime()
   {
-    if (number == 2) return true;
-    if (number % 2 == 0) return false;
+    if (number == 2) 
+      return true;
+    if (number % 2 == 0) 
+      return false;
     for (int i = 3; i < number; i=i+2)
     {
-        if (number % i == 0) return false;
+      if (number % i == 0) 
+        return false;
     }
     return true;
   }
 
-
-
+  // return true if number is a perfect number
   public boolean isPerfect() 
   {
     int sum = 0;
@@ -59,37 +64,38 @@ class MyInteger
         sum = sum + divisor;
       }	
     }
-      if (sum==number) return true; {
-      }
+      if (sum==number) return true; {}
 	return false;
   }
 
+  // return true if number is a perfect square
   public boolean isPerfectSquare() 
   {
     int i;
 
     for(i=0;i<number/2;i++) 
     {
-      if(i*i==number) return true; {
-      }
+      if(i*i==number) return true; {}
     }
   return false;
   }
 
+  // return true if number is prime
   public boolean isPrime(int p)
   {
     int count = 0;
-	for (int i = 1; i <=p; i++)
-  {
-		 if (p % i == 0)
- 			 count++;
-	};
-	if (count == 2)
-		return true;
-	else 
-		return false;
+  	for (int i = 1; i <=p; i++)
+    {
+  		 if (p % i == 0)
+   			 count++;
+  	}
+  	if (count == 2)
+  		return true;
+  	else 
+  		return false;
   }
 
+  //returns all prime factors of the number
   public void primefactors() 
   {
     int p = 2;
@@ -100,7 +106,6 @@ class MyInteger
 	
      	if (temp % p == 0) 
       {
-     		
     		temp = temp / p;
     	} // if
      	else 
@@ -116,13 +121,13 @@ class MyInteger
     }
   }
 
+  // print function
   public void print()
   {
-    System.out.println("Prime:" + isPrime());
-    System.out.println("Even:" + isEven());
-    System.out.println("Odd:" + isOdd());
-    System.out.println("Perfect:" + isPerfect());
-    System.out.println("Perfect Square:" + isPerfectSquare());
-
+    System.out.println("Prime: " + isPrime());
+    System.out.println("Even: " + isEven());
+    System.out.println("Odd: " + isOdd());
+    System.out.println("Perfect: " + isPerfect());
+    System.out.println("Perfect Square: " + isPerfectSquare() + "\n");
   }
 }
