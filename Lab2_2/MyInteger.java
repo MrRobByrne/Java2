d267cd86876f658106931ad8fc9334bab7a19124
Robert Byrne <mr.rob.byrne@gmail.com>
2015-03-28 05:17:56 -0400

    Refactored MyInteger code
    Refactored MyInteger code

--- Lab2_2/MyInteger.java
+++ Lab2_2/MyInteger.java
@@ -1,51 +1,62 @@
-class MyInteger{
+class MyInteger
+{
   private int number;
 
-  public MyInteger(){
+  public MyInteger()
+  {
     number = 0;
   }
 
-  public MyInteger(int number){
+  public MyInteger(int number)
+  {
     this.number = number;
   }
 
-  public String toString(){
+  public String toString()
+  {
     String temp;
     temp = String.valueOf(number);
     return temp;
   }
 
-  public boolean isEven(){
+  public boolean isEven()
+  {
     if (number % 2 == 0)
       return true;
     else if (number % 2 == 1);
       return false;
   }
 
-  public boolean isOdd(){
+  public boolean isOdd()
+  {
     if (number % 2 == 1)
       return true;
     else if (number % 2 == 0);
       return false;
   }
 
-  public boolean isPrime(){
+  public boolean isPrime()
+  {
     if (number == 2) return true;
     if (number % 2 == 0) return false;
-    for (int i = 3; i < number; i=i+2){
+    for (int i = 3; i < number; i=i+2)
+    {
         if (number % i == 0) return false;
-  }
+    }
     return true;
-}
+  }
 
 
 
-  public boolean isPerfect() {
+  public boolean isPerfect() 
+  {
     int sum = 0;
 
-    for(int divisor=1; divisor<number; divisor++){
-      if ((number%divisor)==0){
-	sum = sum + divisor;
+    for(int divisor=1; divisor<number; divisor++)
+    {
+      if ((number%divisor)==0)
+      {
+        sum = sum + divisor;
       }	
     }
       if (sum==number) return true; {
@@ -53,51 +64,60 @@ class MyInteger{
 	return false;
   }
 
-  public boolean isPerfectSquare() {
+  public boolean isPerfectSquare() 
+  {
     int i;
 
-    for(i=0;i<number/2;i++) {
+    for(i=0;i<number/2;i++) 
+    {
       if(i*i==number) return true; {
       }
     }
   return false;
   }
 
-  public boolean isPrime(int p){
+  public boolean isPrime(int p)
+  {
     int count = 0;
-	for (int i = 1; i <=p; i++){
+	for (int i = 1; i <=p; i++)
+  {
 		 if (p % i == 0)
  			 count++;
-	}
-;
+	};
 	if (count == 2)
 		return true;
 	else 
 		return false;
+  }
 
-}
-
-  public void primefactors() {
+  public void primefactors() 
+  {
     int p = 2;
     int temp = number; 
 
-    while(temp  != 1) {
+    while(temp  != 1) 
+    {
 	
- 	if (temp % p == 0) {
- 		
-		temp = temp / p;
-	}
- 	else {
-		p++;System.out.println(number);
-		while(!isPrime(p)) {
-			System.out.print(p + " ");
- 			p++;
-		}
-	}
+     	if (temp % p == 0) 
+      {
+     		
+    		temp = temp / p;
+    	} // if
+     	else 
+      {
+    		p++;System.out.println(number);
+    		
+        while(!isPrime(p)) 
+        {
+    			System.out.print(p + " ");
+     			p++;
+    		} // while
+      } // else
+    }
   }
-}
 
-  public void print(){
+  public void print()
+  {
     System.out.println("Prime:" + isPrime());
     System.out.println("Even:" + isEven());
     System.out.println("Odd:" + isOdd());
---
+++ Lab2_2/MyIntegerDriver.java
@@ -0,0 +1,19 @@
+class MyIntegerDriver
+{
+	public static void main (String args[])
+	{
+	MyInteger number1 = new MyInteger(1);
+	MyInteger number2 = new MyInteger(7);
+	MyInteger number3 = new MyInteger(49);
+	MyInteger number4 = new MyInteger(496);
+
+	System.out.println(number1);
+	    number1.print();
+	System.out.println(number2);
+	    number2.print();
+	System.out.println(number3);
+	    number3.print();
+	System.out.println(number4);
+	    number4.print();
+	}
+}
