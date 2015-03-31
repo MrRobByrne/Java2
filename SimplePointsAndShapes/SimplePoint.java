e77e79cbca81b9742662a144a9b64f9afe37e4d3
Robert Byrne <mr.rob.byrne@gmail.com>
2015-03-31 01:57:39 -0400

    SimplePoint Commented
    SimplePoint Commented with better comments and parameters

--- SimplePointsAndShapes/SimplePoint.java
+++ SimplePointsAndShapes/SimplePoint.java
@@ -1,23 +1,36 @@
-class SimplePoint {
+// SimplePoint class has a constructor SimplePoint(), move(), and toString()
+
+class SimplePoint 
+{
 	private int x;
 	private int y;
 
-  	public SimplePoint() {
+  	public SimplePoint() 
+    {
     	x = 0;
     	y = 0;
   	}
 
-  	public SimplePoint(int x, int y) {
+    // constructor SimplePoint() class
+    // @Params int x, int y
+  	public SimplePoint(int x, int y) 
+    {
     	this.x = x;
     	this.y = y;
   	}
 
-  	public void move(int m, int n) {
+    // move() moves the selected points coordinates to new x and y points
+    // @Params int m, int n ~ (x, y)
+  	public void move(int m, int n) 
+    {
     	x = m;
     	y = n;
   	}
 
-  	public String toString() {
+    // toString()
+    // returns string of the simple points coordinates
+  	public String toString() 
+    {
     	String s = "x: " + x + "\ty: " + y;
     	return s;
   	}
